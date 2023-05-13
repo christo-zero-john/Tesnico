aaa924c6863e96b417dc868dca47c2e90d2b681b
Christo John <10.christojohn@gmail.com>
2023-05-13 15:57:51 +0530

    Commit on 13 05 2023

--- source-code/tesnico.js
+++ source-code/tesnico.js
@@ -1,21 +1,11 @@
-/*
+const {Telegraf} = require('telegraf');
+const bot = new Telegraf('5670886047:AAGWDSCdXB5RymUBT4x_tIK0rJw1Q1JbOac');
 
-  Project started on 16 03 2023
-  Bot first activated on 23 03 2023
-  
-    _________________________________________________________________________________________
-    | Initial real source-code of telegram bot tesnico                                                  | |
-    | all programs are stored inside this file individual programs commented if not in use | |
-    | Main file                                                                            | |
-    |______________________________________________________________________________________| |
-    \________________________________________________________________________________________|
+bot.start((ctx)=>{
+    ctx.reply("Tesnic Welcomes You . . .\nSent /tesnico to start Tesnico : :");
+})
+bot.command(["tesnico"],(ctx)=>{
+    ctx.reply("Welcome . . . \n I am Tesnico a Telegram Bot developed by Tesnic\n Try /requests to see all availanle commands\nTry /help to get help . . .\n\nTry /documentation to see my documentation and to learn more about me . . .\n\nWish ya a Good day <->");
+})
 
-    command list available :
-
-    Go to bit.ly/tesnico-documentation to know more about tesnico
-*/
-
-
-const Telegraf = require('telegraf');
-const BOT_TOKEN=require("./bot-token")
-const bot = new Telegraf(BOT_TOKEN);
+bot.launch();
